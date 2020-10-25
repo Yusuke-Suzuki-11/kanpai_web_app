af482809b4672090af9b42d9815bae14a54c4fd4
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2020-10-25 15:54:06 +0900

    クラステーブル作成

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function class()
+    {
+        return $this->belongsToMany('App\Class');
+    }
 }
---
+++ database/migrations/2020_10_25_055041_create_classes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer("week_day_type");
+            $table->time('start_time');
+            //人数の上限
+            $table->integer('limit_person');
+            // 難しさの上限
+            $table->integer('limit_difficulty＿point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
